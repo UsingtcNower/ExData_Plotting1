3188322adc692aeb4cf709ef7966aea2efb87375
UsingtcNower <nowerzt@gmail.com>
2015-02-08 12:19:12 +0800

    finish plot4

---
+++ plot4.R
@@ -0,0 +1,32 @@
+plot4 <- function() {
+  df <- read.delim("household_power_consumption.txt",sep=";", as.is=TRUE)
+
+  df <- subset(df, Date %in% c("1/2/2007", "2/2/2007"), drop=TRUE)
+  
+  df$Global_active_power <- as.numeric(df$Global_active_power)
+  
+  Sys.setlocale("LC_TIME", "en_US")
+  df$datetime <- as.POSIXlt(paste(df$Date, df$Time), format="%d/%m/%Y %H:%M:%S")
+  
+  png("plot4.png")
+  par(mfrow=c(2,2))
+  #plot
+  with(df, plot(datetime, Global_active_power, type="l", xlab="", 
+                ylab="Global Active Power (kilowatts)"))
+  
+  df$Voltage <- as.numeric(df$Voltage)
+  with(df, plot(datetime, Voltage, type="l", xlab="datetime", 
+                ylab="Voltage"))
+  
+  with(df, plot(datetime, Sub_metering_1, col="black", type="l", xlab="", 
+                ylab="Energy sub metering"))
+  with(df, points(datetime, Sub_metering_2, col="red", type="l"))
+  with(df, points(datetime, Sub_metering_3, col="blue", type="l"))
+  legend("topright", lty=1, lwd=1, col=c("black","red","blue"), 
+         legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+  
+  df$Global_reactive_power <- as.numeric(df$Global_reactive_power)
+  with(df, plot(datetime, Global_reactive_power, type="l", xlab="datetime", 
+                ylab="Global_reactive_power"))
+  dev.off()
+}
